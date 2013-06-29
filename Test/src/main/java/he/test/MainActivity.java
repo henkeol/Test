e866e8649e431f36d129bcc85a964e93a598f606
Henrik Eklund <henrikeklund.96@gmail.com>
2013-06-29 17:24:51 +0200

    Change the code added a onClickListener and more

--- Test/src/main/java/he/test/MainActivity.java
+++ Test/src/main/java/he/test/MainActivity.java
@@ -4,13 +4,12 @@ package he.test;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.view.View;
+import android.view.View.OnClickListener;
 
-
-
-public class MainActivity extends Activity{
+public class MainActivity extends Activity implements OnClickListener{
 
     private Button b1;
     private Button b2;
@@ -20,17 +19,19 @@ public class MainActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         chronometer = (Chronometer)findViewById(R.id.chronometer);
         b1 = (Button)findViewById(R.id.button);
-        b1.setOnClickListener(new OnClickListener());
+        b1.setOnClickListener(this);
         b2 = (Button)findViewById(R.id.button2);
-        b2.setOnClickListener(new OnClickListener());
+        b2.setOnClickListener(this);
         b3 = (Button)findViewById(R.id.button3);
-        b3.setOnClickListener(new OnClickListener());
+        b3.setOnClickListener(this);
+
 
 
     }
+
 
 
     @Override
@@ -40,15 +41,14 @@ public class MainActivity extends Activity{
         return true;
     }
 
-   private class OnClickListener implements View.OnClickListener{
-       @Override
-       public void onClick(View view) {
-           if(view.equals((Button)findViewById(R.id.button))){
-               chronometer.start();
-           }
-           if(view.equals((Button) findViewById(R.id.button2))){
-                chronometer.stop();
-           }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.button){
+            chronometer.start();
+        }
+        if (view.getId() == R.id.button2){
+            chronometer.stop();
         }
     }
 }
